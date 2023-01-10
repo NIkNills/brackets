ad7105f8902935cefd82e9f1998adfd5481d9dbc
Nik Kolesnikov <niknills94@gmail.com>
2023-01-10 21:17:38 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = bracketsConfig.map((i) => i.join(""));
+
+  for (let i = 0; i < arr.length; i++) {
+    if (str.includes(arr[i])) {
+      str = str.replaceAll(arr[i], "");
+      i = -1;
+    }
+  }
+
+  return str.length === 0 ? true : false;
+};
